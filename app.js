dc0ee5f4fb43045f3304884f9857203e19446f24
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-11-01 17:31:10 +0100

    js-practice

---
+++ app.js
@@ -0,0 +1,22 @@
+// this code create a popup message.
+/*
+ the popup message says
+ 'hello'
+*/
+alert('hello');
+
+//console.log (2+2);
+//console.log('some'+'text');
+console.log('item ('+(1+1)+'):$'+ (2095+799)/100);
+//console.log("I'm learning javascript");
+//console.log(alert ('Hello world'));
+//console.log("my name is deji\nwhat\'s yours");
+console.log(`item (${1+1}):$${(2095+799)/100}`);
+//console.log(alert(`somebody
+//something
+//somewhere`));
+
+if (5) {
+  console.log('truthy')
+}
+console.log(Math.round(0.9));
